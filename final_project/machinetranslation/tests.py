d803e7892ade396980525a44ccc692d1cc000330
ironmanmarkyo <yohan.kumar123@gmail.com>
2023-06-21 10:20:52 -0400

    Final Changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -7,7 +7,7 @@ class Testenglish_to_french(unittest.TestCase):
         self.assertEqual(english_to_french("Hello"),"Pepitoooo")
     
     def test2(self):
-        self.assertEqual(english_to_french("You"),"Vous")
+        self.assertNotEqual(english_to_french("You"),"Merci")
     
     def test3(self):
         self.assertEqual(english_to_french("Bonjour"),"Bonjour")
@@ -17,6 +17,6 @@ class Testfrench_to_english(unittest.TestCase):
         self.assertEqual(french_to_english("Bonjour"),"Hello")
     
     def test2(self):
-        self.assertEqual(french_to_english("Toi"),"You")
+        self.assertNotEqual(french_to_english("Toi"),"Bye")
 
 unittest.main()
